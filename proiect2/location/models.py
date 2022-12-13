f477c74ccce9996278de376817d319357d8792a3
kais230 <kaisxd.kx@gmail.com>
2022-12-13 15:42:33 +0200

    mesaj

--- proiect2/location/models.py
+++ proiect2/location/models.py
@@ -5,8 +5,12 @@ from django.db import models
 class Location(models.Model):
 
     city = models.CharField(max_length=100)
-    country = models.CharField(max_length=90)
+    country = models.CharField(max_length=98)
     active = models.BooleanField(default=1)
 
     def __str__(self):
-        return f"{self.city} -> {self.country}"
+        return f"{self.city} ->{self.country}"
+
+
+
+
